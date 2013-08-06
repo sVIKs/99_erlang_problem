07737379e1ad706853a778bf32e0ce90b91f6d6b
sVIKs <sviks@i.ua>
2013-08-06 12:56:24 +0300

    Update p17.erl

--- src/p17.erl
+++ src/p17.erl
@@ -2,5 +2,9 @@
 -export([split/2]).
 
 split(List, Num) -> split(List,Num,[]).
-split([H | T],0,List1) -> [lists:reverse(List1) , T];
+split([_H | T],0,List1) -> [reverse(List1) , T];
 split([H | T],Num,List1) -> split(T,Num-1,[H|List1]).
+
+reverse(List) -> reverse(List, []).
+reverse([], Reverse_List) -> Reverse_List;
+reverse([H | T], Reverse_List) -> reverse(T, [H | Reverse_List]).
