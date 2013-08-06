2b2be27b863ef72630e19ca7f3a9823f3da6459a
sVIKs <sviks@i.ua>
2013-08-06 12:56:04 +0300

    Update p18.erl

--- src/p18.erl
+++ src/p18.erl
@@ -1,6 +1,6 @@
 -module(p18).
 -export([slice/3]).
 
-slice(List,0,0)-> [];
+slice(_List,0,0)-> [];
 slice([H|T],0,NumE) -> [H | slice(T,0,NumE-1)];
-slice([H|T],NumS,NumE) -> slice(T,NumS-1,NumE-1).
+slice([_H|T],NumS,NumE) -> slice(T,NumS-1,NumE-1).
