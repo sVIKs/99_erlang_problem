042104f852400f19777761c7ea826b68dc367b70
sVIKs <sviks@i.ua>
2013-08-07 09:41:46 +0300

    Update p21.erl

--- src/p21.erl
+++ src/p21.erl
@@ -1,5 +1,5 @@
 -module(p21).
--export([insert_at/3]).
+-export([delete/3]).
 
-insert_at(Element, List, 1) -> [Element | List];
-insert_at(Element, [H|T], Num) -> [H | insert_at(Element, T, Num-1)].
+delete(Element, List, 1) -> [Element | List];
+delete(Element, [H|T], Num) -> [H | delete(Element, T, Num-1)].
